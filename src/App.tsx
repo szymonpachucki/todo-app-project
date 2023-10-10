a1b9c31240551ed0757ef60a919650fb30554df9
Szymon Pachucki <szymonpachucki010@gmail.com>
2023-10-10 09:25:41 +0200

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Todo } from './types/Todo';
+import { Errors } from './types/Errors';
+import { FilterOption } from './types/filterOption';
+import { TodoList } from './components/todoList';
+import { InCaseOfError } from './components/inCaseOfError';
+import { InputOfTodos } from './components/inputOfTodos';
+import { Footer } from './components/footer';
 
-function App() {
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<Errors | null>(null);
+  const [filterTodos, setFilterTodos]
+  = useState<FilterOption>(FilterOption.ALL);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleSetFilter = (newFilter: FilterOption) => {
+    setFilterTodos(newFilter);
+  };
+
+  const closeError = () => {
+    setError(null);
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      const newTodoItem: Todo = {
+        id: todos.length + 1,
+        title: newTodo,
+        completed: false,
+        removed: false,
+        editing: false,
+      };
+
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <InputOfTodos
+          setNewTodo={setNewTodo}
+          newTodo={newTodo}
+          addTodo={addTodo}
+          todos={todos}
+          setTodos={setTodos}
+        />
+
+        {todos && (
+          <TodoList
+            todos={todos}
+            filterTodos={filterTodos}
+            setTodos={setTodos}
+          />
+        )}
+
+        {todos.length > 0 && (
+          <Footer
+            handleSetFilter={handleSetFilter}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        )}
+      </div>
+
+      {error && (
+        <InCaseOfError error={error} closeError={closeError} />
+      )}
     </div>
   );
-}
-
-export default App;
+};
